9f6ee2585132080e23bc3dd97d987239ef751e4e
Daniel Zhang <dzhan96@uwo.ca>
2020-02-25 19:04:23 -0500

    Remove all loggers other than minimal

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,118 @@
-'use strict';
+import chalk from 'chalk';
+import log from 'log-update';
+import {ProgressPlugin} from 'webpack';
 
-// // Disable the deprecation warning, popping up in the middle of the process
-// (process as any).noDeprecation = true;
+export function ProgressWebpackPlugin() {
+  const absoluteProjectPath = process.cwd();
+  let startTime = new Date().getTime();
+  let previousStep = 0;
+  let logLine = '';
 
-import CompactLogger from './logger/compact-logger';
-import MinimalLogger from './logger/minimal-logger';
-import VerboseLogger from './logger/verbose-logger';
-import ExpandedLogger from './logger/expanded-logger';
-import {ProgressPlugin as WebpackProgressPlugin} from 'webpack';
+  //  Use the webpack-internal progress plugin as the base of the logger
+  return new ProgressPlugin((progress, message, moduleProgress, _, moduleName) => {
+    logLine = chalk.yellow(`[${Math.round(progress * 100)}%] `);
 
-export type PluginOptions = {
-  format?: 'minimal' | 'compact' | 'expanded' | 'extended' | 'verbose' | 'debug';
-  color?: boolean;
-};
+    // Reset process variables for this run
+    if (previousStep === 0) {
+      startTime = new Date().getTime();
+    }
 
-export type InternalOptions = {
-  format: string;
-  color: boolean;
-};
+    // STEP 1: COMPILATION
+    if (progress >= 0 && progress < 0.1) {
+      // Skip if we jumped back a step, else update the step counter
+      if (previousStep > 1) {
+        return;
+      }
+      previousStep = 1;
+      logLine += chalk.white('Compiling modules ...');
+    }
 
-function getOption(options: PluginOptions, key: string, value: string | boolean): string | boolean {
-  return options && options.hasOwnProperty(key) ? options[key] : value;
+    // STEP 2: BUILDING
+    if (progress >= 0.1 && progress <= 0.7) {
+      // Skip if we jumped back a step, else update the step counter
+      if (previousStep > 2) {
+        return;
+      }
+      previousStep = 2;
+
+      // Log progress line
+      logLine += chalk.white('Building modules ...');
+
+      // Log additional information (if possible)
+      if (moduleName !== undefined && moduleProgress !== undefined) {
+        let betterModuleName = moduleName;
+
+        // Only show the file that is actually being processed (and remove all details about used loaders)
+        if (betterModuleName.indexOf('!') !== -1) {
+          let splitModuleName = betterModuleName.split('!');
+          betterModuleName = splitModuleName[splitModuleName.length - 1];
+        }
+
+        // Transform absolute paths into relative ones (to shorten the so so incredible long path)
+        if (betterModuleName.indexOf(absoluteProjectPath) !== -1) {
+          betterModuleName = betterModuleName
+            .split(`${absoluteProjectPath}`)[1] // Transform absolute path to relative one
+            .substring(1); // Remove leading path slash
+        }
+
+        // Improve the path presentation further by enforcing style consistency and removing unnecessary details
+        betterModuleName = betterModuleName
+          .replace(/\\/g, '/')
+          .replace('./', '')
+          .replace('multi ', '');
+
+        // Add extra details about whether the currently processed module is an internal or external one
+        if (betterModuleName.startsWith('node_modules')) {
+          betterModuleName = `${betterModuleName} ~ external`;
+        }
+        if (betterModuleName.startsWith('src')) {
+          betterModuleName = `${betterModuleName} ~ internal`;
+        }
+
+        const [betterModulesDone, betterAllModules] = moduleProgress.split('/');
+        const moduleDetails = `${betterModulesDone} of ${betterAllModules} :: ${betterModuleName}`;
+        logLine += chalk.grey(` (${moduleDetails})`);
+      }
+    }
+
+    // STEP 3: OPTIMIZATION
+    if (progress > 0.7 && progress < 0.95) {
+      // Skip if we jumped back a step, else update the step counter
+      if (previousStep > 3) {
+        return;
+      }
+      previousStep = 3;
+
+      // Log progress line (with sub-progress indicator)
+      logLine += chalk.white('Optimizing modules ...');
+      const formattedMessageExtra = progress === 0.91 ? ' -- may take a while' : '';
+      logLine += chalk.grey(` (${message}${formattedMessageExtra})`);
+    }
+
+    // STEP 4: EMIT
+    if (progress >= 0.95 && progress < 1) {
+      // Skip if we jumped back a step, else update the step counter
+      if (previousStep > 4) {
+        return;
+      }
+      previousStep = 4;
+      logLine += chalk.white('Emiting files ...');
+    }
+
+    // STEP 5: FOOTER
+    if (progress === 1) {
+      // Calculate process time
+      previousStep = 0;
+      const finishTime = new Date().getTime();
+      const processTime = ((finishTime - startTime) / 1000).toFixed(3);
+      logLine = chalk.white(`[Webpack] Finished in ${processTime} seconds.`); // Overwrite
+    }
+
+    log(logLine);
+    if (progress === 1) {
+      log.done();
+    }
+  });
 }
 
-/**
- * Simple Progress Plugin for Webpack
- *
- * @param options - Custom options
- */
-export default function SimpleProgressWebpackPlugin(options: PluginOptions) {
-  const internalOptions: InternalOptions = {
-    format: getOption(options, 'format', 'compact'),
-    color: getOption(options, 'color', true),
-  };
-
-  // Return the correct progress plugin
-  switch (internalOptions.format) {
-    case 'minimal':
-      return MinimalLogger(internalOptions);
-    case 'expanded':
-    case 'extended':
-      return ExpandedLogger(internalOptions);
-    case 'verbose':
-    case 'debug':
-      return VerboseLogger(internalOptions);
-    case 'compact':
-    default:
-      return CompactLogger(internalOptions);
-  }
-}
+export default ProgressWebpackPlugin;
